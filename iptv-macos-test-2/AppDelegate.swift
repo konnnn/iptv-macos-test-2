25cabee335cb336e5063e5ac5ce0c3e9b076fbfc
Евгений Конкин <evgeniykonkin@gmail.com>
2019-04-14 22:39:01 +0400

    Initial Commit

---
+++ iptv-macos-test-2/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  iptv-macos-test-2
+//
+//  Created by Evgeny Konkin on 14.04.2019.
+//  Copyright © 2019 Evgeny Konkin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ iptv-macos-test-2/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  iptv-macos-test-2
+//
+//  Created by Evgeny Konkin on 14.04.2019.
+//  Copyright © 2019 Evgeny Konkin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
